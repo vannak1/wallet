34e73da057e06cdefc9401f0cc3af5fd9617547f
Vannak Chhay <vannak@Mac-mini.local>
2018-02-09 13:14:55 -0500

    integrated AWS Amplify login

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+            <input type="file" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" multiple>
+        <div className="preview">
+          <p>No files currently selected for upload</p>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
       </div>
     );
   }
